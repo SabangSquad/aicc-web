ec2035d5f7223d7488011ffcf9eaa520d704f8c6
optshj <optshj@gmail.com>
2026-04-05 18:23:59 +0900

    ♻️ [Refactor] entities/manuals 폴더 삭제

--- src/entities/manual/api/api.ts
+++
@@ -1,8 +0,0 @@
-import { http } from '@/shared/lib/http';
-import { Manual } from '@/shared/types/manual';
-
-export const ManualsAPI = {
-  getManuals: (category: string) => {
-    return http.get<{ data: Manual[] }>(`/manuals?category=${category}`).then(res => res.data || []);
-  },
-};
--- src/entities/manual/hooks/useManualQuery.tsx
+++
@@ -1,6 +0,0 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { queryOptions } from '../api/queryOptions';
-
-export function useManuals(category: string) {
-  return useSuspenseQuery(queryOptions.manuals(category));
-}
