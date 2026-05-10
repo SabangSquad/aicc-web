87ee8e13156ddcf735fb2f7aa4d2d1ea3d1782fd
optshj <optshj@gmail.com>
2026-05-10 15:23:47 +0900

    ✏️ [Fix] 오탈자 수정

--- src/entities/auth/api/logout.ts
+++ src/entities/auth/api/logout.ts
@@ -1,7 +1,7 @@
 'use server';
 import { cookies } from 'next/headers';
 
-export async function logoutAction() {
+export async function logout() {
   const cookieStore = await cookies();
 
   cookieStore.set('connect.sid', '', {
